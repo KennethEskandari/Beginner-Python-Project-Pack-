078be3a4cd73899c7b95a1dad425c36628fd04e4
Kenneth Eskandari <Kenneth@EskandariAnalysis.xyz>
2025-04-15 14:27:11 -0400

    0.9

---
+++ .history/9.FlashcardApp/FlashcardApp_20250415140352.py
@@ -0,0 +1,43 @@
+import json
+import random 
+
+#Loading Flashcards 
+def load_flashcards(filename):
+    try:
+        with open(filename,'r') as f:
+            return json.load(f)
+    except FileExistsError:
+        return []
+    
+#Saving Flashcards
+def save_flashcards(flashcards,filename):
+    with open(filename,'w') as f:
+        json.dump(flashcards,f)
+
+#Quiz 
+def take_quiz(flashcards):
+    if not flashcards:
+        print('No Flashcards!')
+        return
+    
+    random.shuffle(flashcards)
+    for card in flashcards:
+        answer = input(f'Q:{card['question']} \nYour Answer: ')
+        if answer.strip().lower() == card['answer'].strip().lower():
+            print("Correct!\n")
+        else:
+            print('Incorrect!\n')
+
+    #Adding Flashcard
+    def add_flashcard(flashcards):
+        question = input('Enter Question')
+        answer = input('Enter Answer')
+        flashcards.append({'questions': question, 'answer': answer})
+        print("Flashcard Added!\n")
+
+    #Menu 
+    def main():
+        filename = flashcards.json
+        flashcards = load_flashcards(filename)
+
+        
---
+++ .history/9.FlashcardApp/FlashcardApp_20250415140459.py
@@ -0,0 +1,49 @@
+import json
+import random 
+
+#Loading Flashcards 
+def load_flashcards(filename):
+    try:
+        with open(filename,'r') as f:
+            return json.load(f)
+    except FileExistsError:
+        return []
+    
+#Saving Flashcards
+def save_flashcards(flashcards,filename):
+    with open(filename,'w') as f:
+        json.dump(flashcards,f)
+
+#Quiz 
+def take_quiz(flashcards):
+    if not flashcards:
+        print('No Flashcards!')
+        return
+    
+    random.shuffle(flashcards)
+    for card in flashcards:
+        answer = input(f'Q:{card['question']} \nYour Answer: ')
+        if answer.strip().lower() == card['answer'].strip().lower():
+            print("Correct!\n")
+        else:
+            print('Incorrect!\n')
+
+    #Adding Flashcard
+    def add_flashcard(flashcards):
+        question = input('Enter Question')
+        answer = input('Enter Answer')
+        flashcards.append({'questions': question, 'answer': answer})
+        print("Flashcard Added!\n")
+
+    #Menu 
+    def main():
+        filename = flashcards.json
+        flashcards = load_flashcards(filename)
+
+        while True:
+             print("\n--- Flashcard Quiz App ---")
+             print("1. Take a quiz")
+             print("2. Add a flashcard")
+             print("3. Save flashcards")
+             print("4. Exit")
+
